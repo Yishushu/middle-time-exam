1e72cf94a77442dbfb6521ab5c5bb7119b436b66
your_name <1594051054@qq.com>
2018-05-04 16:04:42 +0800

    iddle time exam2

--- meituan/views.py
+++ meituan/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def home_page(request):
 
-    if request.method == 'order':
-        address = request.order.get('order')
-        order = request.order.get("address")
+    if request.method == 'POST':
+        address = request.POST.get('order')
+        order = request.POST.get("address")
 
         print(address * 10)
         return render(request, 'meituan/home_page.html', {'address': address, 'order': order})
