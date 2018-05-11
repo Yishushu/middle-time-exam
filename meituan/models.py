d0881f5db1c4ec594d4edf881c0ac48827ef3d8c
your_name <1594051054@qq.com>
2018-05-11 16:39:29 +0800

    fixed models, site working, next do for loop

--- meituan/models.py
+++ meituan/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+     address = models.CharField(max_length=180, null=True, blank=True)
+     meal = models.CharField(max_length=180, null=True, blank=True)
+
+# Create your models here.
--- meituan/views.py
+++ meituan/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-
+from . models import Order
 def home_page(request):
-
     if request.method == 'POST':
-        address = request.POST.get('order')
-        order = request.POST.get("address")
+        our_address = request.POST.get('address')
+        our_user = request.POST.get("order")
+        address_obj = Order.objects.create(
+            address=our_address,
+            meal=our_user
+        )
 
-        print(address * 10)
-        return render(request, 'meituan/home_page.html', {'address': address, 'order': order})
-
+        return render(request, 'meituan/home_page.html', {'order': address_obj,})
     return render(request, 'meituan/home_page.html')
